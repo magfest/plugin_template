6246449fdb6afccea4d2489741f06af2882d654a
Rob Ruana <rob@robruana.com>
2018-02-21 11:57:03 -0500

    Runs flake8 against entire project

--- plugin_template/__init__.py
+++ plugin_template/__init__.py
@@ -1,10 +1,18 @@
-from uber.common import *
-from ._version import __version__
-from .config import *
-from .models import *
-from .model_checks import *
-from .automated_emails import *
+from os.path import join
+
+from uber.jinja import template_overrides
+from uber.utils import mount_site_sections, static_overrides
+
+from ._version import __version__  # noqa: F401
+from .config import config
+
 
 static_overrides(join(config['module_root'], 'static'))
 template_overrides(join(config['module_root'], 'templates'))
 mount_site_sections(config['module_root'])
+
+
+# These need to come last so they can make use of config properties
+from .models import *  # noqa: F401,E402,F403
+from .model_checks import *  # noqa: F401,E402,F403
+from .automated_emails import *  # noqa: F401,E402,F403
--- plugin_template/config.py
+++ plugin_template/config.py
@@ -1,4 +1,6 @@
-from uber.common import *
+from sideboard.lib import parse_config
+from uber.config import c
+
 
 config = parse_config(__file__)
 c.include_plugin_config(config)
