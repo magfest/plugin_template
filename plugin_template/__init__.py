5b7911d41201c6bb6482ea5ebdee077746e8acdb
Rob Ruana <rob@robruana.com>
2018-03-09 17:50:29 -0500

    Adds example on_load() function.

--- plugin_template/__init__.py
+++ plugin_template/__init__.py
@@ -16,3 +16,12 @@ mount_site_sections(config['module_root'])
 from .models import *  # noqa: F401,E402,F403
 from .model_checks import *  # noqa: F401,E402,F403
 from .automated_emails import *  # noqa: F401,E402,F403
+
+
+def on_load():
+    """
+    Called by sideboard when the plugin is loaded.
+
+    This function is optional, and can be removed if not needed.
+    """
+    pass
